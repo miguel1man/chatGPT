5f3e8a1ba350e1de0158e0769c2796383a131f32
Miguel Angel <miguelangelhh17@gmail.com>
2023-02-08 11:45:48 -0500

    create app.js

---
+++ pages/components/app.js
@@ -0,0 +1,98 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "../index.module.css";
+
+const giphyApi = process.env.NEXT_PUBLIC_GIPHY_API_KEY;
+
+export default function Home() {
+
+  const [animalInput, setAnimalInput] = useState("");
+  const [result, setResult] = useState();
+  const [inputValue, setInputValue] = useState("");
+  const [showInputValue, setShowInputValue ] = useState(false);
+  const [urlImage1, SeturlImage1] = useState();
+  const [urlImage2, SeturlImage2] = useState();
+  const [urlImage3, SeturlImage3] = useState();
+  const [showImages, setShowImages] = useState(false);
+
+  let splitResponse;
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ animal: animalInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      splitResponse = data.result.split(/[\s,]+/).filter(Boolean);
+      console.log(splitResponse);
+      setAnimalInput("");
+
+      const joinResponse = splitResponse.join("+");
+      const PREFIX_IMAGE_URL = `https://api.giphy.com/v1/gifs/search?q=${joinResponse}&api_key=${giphyApi}&limit=3`;
+
+      fetch(PREFIX_IMAGE_URL)
+        .then(response => response.json())
+        .then(res => {
+          SeturlImage1(res.data[0].images.fixed_height.url);
+          SeturlImage2(res.data[1].images.fixed_height.url);
+          SeturlImage3(res.data[2].images.fixed_height.url);
+          setShowImages(true);
+        })
+        .catch(e => {
+          console.error(e);
+          alert(e.message);
+        });
+      
+      console.log(result);
+
+    } catch(error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>GPT Reacciona</title>
+        <link rel="icon" href="/bot.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/bot.png" className={styles.icon} />
+        <h3>Escribe...</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="animal"
+            placeholder="... y chatGPT reaccionará"
+            value={animalInput}
+            onChange={(e) => {
+              setAnimalInput(e.target.value);
+            }}
+          />
+          <input type="submit" value="Reaccionar" onClick={() => {
+            setShowImages(false);
+            setInputValue(animalInput)
+            setShowInputValue(true);
+          }} />
+        </form>
+        {showInputValue && <p>Situación: {inputValue}</p>}
+        {showImages && urlImage1 && <img className="imageResponse" src={urlImage1} alt='Primera respuesta de chatGPT' />}
+        {showImages && urlImage2 && <img className="imageResponse" src={urlImage2} alt='Segunda respuesta de chatGPT' />}
+        {showImages && urlImage3 && <img className="imageResponse" src={urlImage3} alt='Tercera respuesta de chatGPT' />}
+      </main>
+    </div>
+  );
+}
